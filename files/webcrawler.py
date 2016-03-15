769fd667e762767da238176a4abb106de2d2a3ff
Thorbjørn Bak Jensen <thorbjornbakjensen@gmail.com>
2016-03-15 22:54:28 +0100

    WebcrawlerSucces!
    Web crawler gets the links from the page provided with the given class
    name - here Itemname

--- files/webcrawler.py
+++ files/webcrawler.py
@@ -1,17 +1,26 @@
+#Remember to install Requests and BeautifulSoup prior to first run.
+#Note - still have to find out how to push it to the test page
+
 import requests
 from bs4 import BeautifulSoup
 
 def trade_spider(max_pages):
     page = 1
     while page <= max_pages:
-        url = "https://thenewboston.com/videos.php?cat=98&video=20144" #+ str(page)
-        source_code = request.get(url)
+        print("Copying adress")
+	url = "https://thenewboston.com/videos.php?cat=98&video=20144" #+ str(page)
+        source_code = requests.get(url)
         plain_text = source_code.text
-        soup = BeautifulSoup(plain_text)
-        for link in soup.findAll("a", {"class": "itemname"}):
-            href = link.get("href")
-            print("href")
+        print("Ready the soup")
+	soup = BeautifulSoup(plain_text)
+	print("Run the machine")
+        for link in soup.findAll("a", {"class": "title"}):
+	    href = link.get("href")
+	    print(href)
+	    #print("page")
+	page = 2 #kunstigt sat stopklods
 trade_spider(1)
+
 def get_single_item_data(item_url):
     source_code = request.get(item_url)
     plain_text = source_code.text
